6dbecd993d0e82fdd278b0f98f1e8e7a88c8b537
Keunsuk <artist0608@hanmail.net>
2022-10-22 09:26:34 +0000

    genblock and generator wip

--- seismogen.py
+++ seismogen.py
@@ -8,32 +8,73 @@ class GenBlock(torch.nn.Module):
     '''
         this block gets a 1-D array (length 400 latent variable Z[:400] ~ Normal(0,1))
     '''
-    def __init__(self, device:str):
-        super(GenBlock, self).__init__(self)
-        self.z_tconv = torch.nn.ConvTranspose1d(in_channels=1, out_channels=16, kernel_size=128, stride=4, padding=62)
-        self.y_linear = torch.nn.Linear(in_features=1, out_features=400)
-        self.conv1 = torch.nn.Conv1d(in_channels=32, out_channels)
-        self.conv2 = torch.nn.Conv1d()
-        self.conv3 = torch.nn.Conv1d()
-    
-    def forward(self, y:int):
-        z = np.random.normal(size=(1, 400))
-        z = torch.Tensor(z)
-        z = self.tconv(z)
+    def __init__(self, length:int=1600):
+        super(GenBlock, self).__init__()
+        self.z_tconv = torch.nn.ConvTranspose1d(in_channels=1, out_channels=16, kernel_size=128, stride=4)
         
-        y = torch.Tensor([[torch.float(y)]])
+        # TODO :  the detail of whether how the size is re-operated so that it is of size 1600.
+        
+        self.z_upsample = torch.nn.Upsample(size=length)
+        self.z_relu = torch.nn.ReLU()
+        self.z_BN = torch.nn.BatchNorm1d(1)
+        
+        self.y_linear = torch.nn.Linear(in_features=1, out_features=400)
+        self.y_tconv = torch.nn.ConvTranspose1d(in_channels=1, out_channels=16, kernel_size=128, stride=4)
+        self.y_relu = torch.nn.ReLU()
+        self.y_BN = torch.nn.BatchNorm1d(1)
+        
+        self.conv1 = torch.nn.Conv1d(in_channels=32, out_channels=32, kernel_size=128, stride=1)
+        self.upsample1 = torch.nn.Upsample(size=length)
+        self.relu1 = torch.nn.ReLU()
+        self.BN1 = torch.nn.BatchNorm1d(32)
+        
+        self.conv2 = torch.nn.Conv1d(in_channels=32, out_channels=32, kernel_size=128, stride=1)
+        self.upsample2 = torch.nn.Upsample(size=length)
+        self.relu2 = torch.nn.ReLU()
+        self.BN2 = torch.nn.BatchNorm1d(32)
+        
+        self.conv3 = torch.nn.Conv1d(in_channels=32, out_channels=1, kernel_size=128, stride=1)
+        self.upsample3 = torch.nn.Upsample(size=length)
+            
+    def forward(self, y:int):
+        '''
+            input y corresponds to the concept of label, which tells whether it is a positive/negative sample.
+        '''
+        z = np.random.normal(size=(1, 400))
+        z = torch.Tensor([z])
+        z = self.z_tconv(z)
+        z = self.z_upsample(z)
+        
+        y = torch.Tensor([[[float(y)]]])
+        
+        y = self.y_linear(y)
+        y = torch.y
         x = torch.cat([y, z], dim=1)
         
         x = self.conv1(x)
-        x = self.conv2(x)
-        x = self.conv3(x)
+        x = self.upsample1(x)
+        x = self.relu1(x)
+        x = self.BN1(x)
         
-    
+        x = self.conv2(x)
+        x = self.upsample2(x)
+        x = self.relu2(x)
+        x = self.BN2(x)
+        
+        x = self.conv3(x)
+        x = self.upsample3(x)
+
+        return x
 class GenNetwork(torch.nn.Module):
     def __init__(self, ):
-        super(GenNetwork, self).__init__(self)
-        pass
+        super(GenNetwork, self).__init__()
+        
         
     def forward(self, z):
         pass
 
+
+if __name__ == '__main__':
+    gblock = GenBlock()
+    x_tilde = gblock(1)
+    print(x_tilde.shape)
